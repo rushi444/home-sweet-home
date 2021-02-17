a248825b50fcc3310f1de550186f2b6f46e29b4d
rushi <rushi44@gmail.com>
2021-02-16 20:49:31 -0800

    auth redirects

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps, NextApiRequest } from 'next'
+
 import { FirebaseAuth } from 'src/components/FirebaseAuth'
 import { Layout } from 'src/components/Layout'
+import { loadIdToken } from 'src/lib/auth/firebaseAdmin'
 
 const Auth = () => {
   return (
@@ -7,6 +10,18 @@ const Auth = () => {
       <FirebaseAuth />
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest)
+
+  if (uid) {
+    res.setHeader('location', '/')
+    res.statusCode = 302
+    res.end()
+  }
+
+  return { props: {} }
 }
 
 export default Auth
