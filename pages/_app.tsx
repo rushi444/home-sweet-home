7c9d3f804432e8e3cc6e6606c7288d323ab9b267
rushi <rushi44@gmail.com>
2021-02-21 16:03:00 -0800

    create apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,25 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client'
 
 import { AuthProvider } from 'src/lib/auth/useAuth'
 import { theme } from 'src/lib/theme'
+import { useApollo } from 'src/lib/apollo'
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const client = useApollo()
   return (
     <AuthProvider>
-      <Head>
-        <title>Home Sweet Home</title>
-      </Head>
-      <ChakraProvider theme={theme}>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Home Sweet Home</title>
+        </Head>
+        <ChakraProvider theme={theme}>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
     </AuthProvider>
   )
 }
